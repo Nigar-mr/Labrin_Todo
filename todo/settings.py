565f8b687ab938f528ddd7b344efea0022af76d8
nigar__ <47503981+Nigar-mr@users.noreply.github.com>
2019-09-14 20:25:14 +0400

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -48,8 +48,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "muradlinigar@gmail.com"
-EMAIL_HOST_PASSWORD ="twmgumxdnqctiyqw"
+EMAIL_HOST_USER = "your email"
+EMAIL_HOST_PASSWORD ="your password"
 
 CELERY_RESULT_BACKEND = 'django-db'
 
